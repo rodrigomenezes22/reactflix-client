7b50e23f6d0595a29e0519a6f5ca02195ac4ac4a
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-02-23 23:10:13 +0100

    footer and videos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home'
+import Footer from './Components/Footer'
+import Videos from './Components/Videos'
+
 function App() {
   return (
     <div className="App">
+      <header>
 
+      </header>
       <main>
         <Routes>
-        <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+
         </Routes>
       </main>
+      <Videos />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
