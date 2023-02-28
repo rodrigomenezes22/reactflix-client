7b8fddfabbb8c504af0ac520aaee036662e2a5c1
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-02-28 22:45:58 +0100

    fixes merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from './Components/Home'
-import Footer from './Components/Footer'
-import Videos from './Components/Videos'
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Videos from './Components/Videos';
 import CmsVideos from './Components/CmsVideos';
-import Home from "./Components/Home";
 import MoviesList from "./Components/MoviesList";
 
 
@@ -15,12 +14,6 @@ function App() {
       <header>
 
       </header>
-      <main>
-        <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/adm-videos" element={ <CmsVideos />} />
-
-
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
