2f449e7edec96ad16c646e055852091dbe28f3dd
Priyanka <priyankadavara@gmail.com>
2023-03-01 13:02:19 +0100

    - removed local data - fetch data from DB

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/moviesList" element={<MoviesList />} />
+          <Route path="/api/movies" element={<MoviesList />} />
         </Routes>
       </main>
     </div>
